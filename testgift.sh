1d974ae47bf9718304bef18303b272cf3504d26f
Anthony Starks <ajstarks@gmail.com>
2014-07-26 09:42:50 -0400

    Create testgift.sh

---
+++ testgift.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+input=rocksky.jpg
+./gift -blur=3 $input				> blur.jpg
+./gift -brightness=30 $input		> bright30.jpg
+./gift -brightness=-30 $input		> bright-30.jpg
+./gift -contrast=30 $input			> contrast30.jpg
+./gift -contrast=-30 $input			> contrast-30.jpg
+./gift -crop=90,90,250,250 $input	> crop.jpg
+./gift -edge $input					> edge.jpg
+./gift -emboss $input				> emboss.jpg
+./gift -fliph $input				> fliph.jpg
+./gift -flipv $input				> flipv.jpg
+./gift -gamma=1.5 $input			> gamma.jpg
+./gift -gray $input					> gary.jpg
+./gift -hue=45 $input				> hue45.jpg
+./gift -hue=-45 $input				> hue-45.jpg
+./gift -invert $input				> invert.jpg
+./gift -max=5 $input				> max.jpg
+./gift -mean=5 $input				> mean.jpg
+./gift -median=5 $input				> median.jpg
+./gift -min=5 $input				> min.jpg
+./gift -resize=200,0 $input			> resize.jpg
+./gift -rotate=90 $input			> rotate90.jpg
+./gift -rotate=180 $input			> rotate180.jpg
+./gift -rotate=270 $input			> rotate270.jpg
+./gift -saturation=50 $input		> sat50.jpg
+./gift -saturation=-50 $input		> sat-50.jpg
+./gift -sepia=100 $input			> sepia.jpg
+./gift -sigmoid=0.5,5.0 $input		> sigmoid.jpg
+./gift -transpose $input			> transpose.jpg
+./gift -transverse $input			> transverse.jpg
+./gift -unsharp=1.0,1.5,0.0 $input	> unsharp.jpg
