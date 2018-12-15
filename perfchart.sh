e0431947a143d3055d612e480f6ba96c0412db3f
Anthony Starks <ajstarks@gmail.com>
2018-12-15 07:54:21 -0500

    Update perfchart.sh
    use dchart

--- perfchart.sh
+++ perfchart.sh
@@ -4,8 +4,10 @@ then
 	echo "specify an image file" 2>&1
 	exit 1
 fi
-sh -x timegift.sh $1 2> giftime
-awk '$0  ~ /gift/ {printf "%s\t", $3} $1 == "real" {print substr($2, 3, 5)}' giftime  | 
+t=/tmp/perfchart.$$
+sh -x timegift.sh $1 2> $t
+awk '$0  ~ /gift/ {printf "%s\t", $3} $1 == "real" {print substr($2, 3, 5)}' $t | 
 (echo "# `ims $1`"; sort -k2 -nr) | 
-lchart -left 20 -top 90 -layout h -datafmt %0.3f -textsize 1.4 -ls 1.6 | pdi > giftime.pdf
+dchart -left 20 -top 90 -hbar -datafmt %0.3f -textsize 1.4 -ls 1.6 -color lightgray | pdi > giftime.pdf
 open giftime.pdf
+rm $t
